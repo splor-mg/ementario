c641bdd89c1229669c58ca971200b79daf410c56
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-25 16:01:45 -0300

    adiciona infra

---
+++ scripts/transform.R
@@ -0,0 +1,12 @@
+read_yaml <- function(filepath, property) {
+  data <- yaml::yaml.load_file(filepath)
+  data <- data.table::rbindlist(data)
+  data
+}
+
+data <- list.files(path = "classificador/fonte-recurso/", full.names = TRUE) |> 
+        purrr::map(read_yaml) |> 
+        data.table::rbindlist()
+
+data.table::fwrite(data, "data/fonte-recurso-hist.csv")
+data.table::fwrite(data[data$dt_fim_vigencia == "9999-12"], "data/fonte-recurso.csv")
