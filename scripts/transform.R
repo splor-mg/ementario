1bdc94c92b08828f6b8a22dd869c7fdc46ec98ef
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-25 11:24:06 -0300

    remove experiments

--- scripts/transform.R
+++ scripts/transform.R
@@ -1,18 +1,12 @@
 read_yaml <- function(filepath, property) {
   data <- yaml::yaml.load_file(filepath)
-  data <- data.table::rbindlist(data[[property]])
+  data <- data.table::rbindlist(data)
   data
 }
 
 data <- list.files(path = "content/fonte-recurso/", full.names = TRUE) |> 
-        purrr::map(read_yaml, property = "hist") |> 
+        purrr::map(read_yaml) |> 
         data.table::rbindlist()
 
 data.table::fwrite(data, "data/fonte-recurso-hist.csv")
-data.table::fwrite(data[data$valid_to == '9999-12'], "data/fonte-recurso.csv")
-
-data_mappings <- list.files(path = "content/fonte-recurso/", full.names = TRUE) |> 
-  purrr::map(read_yaml, property = "mappings") |> 
-  data.table::rbindlist(fill = TRUE)
-
-data.table::fwrite(data_mappings, "data/fonte-recurso-mappings.csv")
+data.table::fwrite(data[data$valid_to == "9999-12"], "data/fonte-recurso.csv")
