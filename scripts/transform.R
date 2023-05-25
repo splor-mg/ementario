3f2c43d1baa840c50816a1730b7e89706dc04e6b
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-24 21:05:32 -0300

    add extra outputs in data/

--- scripts/transform.R
+++ scripts/transform.R
@@ -1,13 +1,18 @@
-read_yaml <- function(filepath) {
+read_yaml <- function(filepath, property) {
   data <- yaml::yaml.load_file(filepath)
-  data <- data.table::rbindlist(data[["hist"]])
-  metadata <- file.info(filepath)
-  data$updated_at <- metadata[["mtime"]]
+  data <- data.table::rbindlist(data[[property]])
   data
 }
 
 data <- list.files(path = "content/fonte-recurso/", full.names = TRUE) |> 
-        lapply(read_yaml) |> 
+        purrr::map(read_yaml, property = "hist") |> 
         data.table::rbindlist()
 
-data.table::fwrite(data, "data/fonte-recurso.csv")
+data.table::fwrite(data, "data/fonte-recurso-hist.csv")
+data.table::fwrite(data[data$valid_to == '9999-12'], "data/fonte-recurso.csv")
+
+data_mappings <- list.files(path = "content/fonte-recurso/", full.names = TRUE) |> 
+  purrr::map(read_yaml, property = "mappings") |> 
+  data.table::rbindlist(fill = TRUE)
+
+data.table::fwrite(data_mappings, "data/fonte-recurso-mappings.csv")
