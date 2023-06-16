b258e80254ea753dea9aa5d9fb769c750c9efe89
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 20:07:52 -0300

    enrich multiple columns

--- scripts/utils.py
+++ scripts/utils.py
@@ -16,14 +16,14 @@ def time_to_decimal(time_str):
     year, month = map(int, time_str.split('-'))
     return Decimal(year) + Decimal(month) / 13
 
-def non_equi_join(tab1, tab2, field_tab1, field_tab2):
+def non_equi_join(tab1, tab2, field_tab1, field_tab2, new_column):
     for record1 in tab1:
         for record2 in tab2:
             valid_from = time_to_decimal(record2['valid_from'])
             valid_to = time_to_decimal(record2['valid_to'])
             valid_ref = time_to_decimal(record1['valid_ref'])
             if record1[field_tab1] == record2[field_tab2] and valid_from <= valid_ref <= valid_to:
-                yield {**record1, f'fonte_desc': record2['desc']}  # merge dictionaries in Python 3.5+
+                yield {**record1, f'{new_column}_desc': record2['desc']}  # merge dictionaries in Python 3.5+
 
 def enrich_resource(resource):
     data = resource.read_rows()
@@ -32,5 +32,5 @@ def enrich_resource(resource):
         foreign_resource_name = foreign_key['reference']['resource']
         foreign_resource_field_name = foreign_key['reference']['fields'][0]
         foreign_resource_data = foreign_package.get_resource(foreign_resource_name).read_rows()
-        result = non_equi_join(data, foreign_resource_data, f'{foreign_resource_name}.{foreign_resource_field_name}', foreign_resource_field_name)
-    return result
+        data = list(non_equi_join(data, foreign_resource_data, foreign_key['fields'], foreign_resource_field_name, foreign_resource_name))
+    return data
