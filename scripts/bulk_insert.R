941aaf4d6da9d5f9392629c13a945f8b24ff26ce
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 15:28:54 -0300

    adiciona estrutura de pastas para referenciacao

--- scripts/bulk_insert.R
+++ scripts/bulk_insert.R
@@ -14,22 +14,14 @@ loa_rec <- loa_rec[, .(valid_ref, FONTE_COD, FONTE_DESC)]
 
 fontes <- rbind(exec_rec, loa_rec)
 fontes <- fontes[, .(valid_from = min(valid_ref), valid_to = max(valid_ref)), by = .(FONTE_COD, FONTE_DESC)][order(FONTE_COD)]
-names(fontes) <- c("cod", "desc", "dt_inicio_vigencia", "dt_fim_vigencia")
+names(fontes) <- c("cod", "desc", "valid_from", "valid_to")
 
-fontes[dt_fim_vigencia == "2023-00", dt_fim_vigencia := "9999-12"]
+fontes[valid_to == "2023-00", valid_to := "9999-12"]
 
 fontes$cod <- as.integer(fontes$cod)
 
-fontes <- data.frame(cod = c(10L, 20L, 60L), 
-                     desc = c("Recursos Ordinarios", "Recursos Vinculados", "Recursos Diretamente Arrecadados"),
-                     dt_inicio_vigencia = "2002-00",
-                     dt_fim_vigencia = "9999-12")
+fontes[, seq := seq_len(.N) , cod]
+setcolorder(fontes, c("cod", "seq", "desc", "valid_from", "valid_to"))
 
-fontes |>
-  dplyr::group_by(cod) |>
-  dplyr::group_split() |>
-  purrr::walk(~{
-    file_name <- paste0("classificador/fonte-recurso/", formatC(.$cod[1], width = 2, flag = "0"), ".yml")
-    yaml::write_yaml(., file_name, column.major = FALSE, indent.mapping.sequence = TRUE)
-  })
+write_yaml(fontes, "classificador/fonteRecurso/lookup.yaml", column.major = FALSE, indent.mapping.sequence = TRUE)
 
