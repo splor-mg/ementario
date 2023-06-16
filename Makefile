51dc5cb02ebe79c3e2f6c28c1b18ad3f730c60b6
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 16:40:13 -0300

    add data package for reference data

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
+all: fonte data/fonte-recurso-temporal.csv
+
 fonte: classificador/fonteRecurso/cod.json classificador/fonteRecurso/desc.json
 
 classificador/fonteRecurso/%.json: classificador/fonteRecurso/%.yaml
 	cat $^ | yq -o=json > $@
+
+data/fonte-recurso-temporal.csv: classificador/fonteRecurso/lookup.yaml
+	Rscript scripts/transform.R
--- scripts/transform.R
+++ scripts/transform.R
@@ -1,12 +1,3 @@
-read_yaml <- function(filepath, property) {
-  data <- yaml::yaml.load_file(filepath)
-  data <- data.table::rbindlist(data)
-  data
-}
-
-data <- list.files(path = "classificador/fonte-recurso/", full.names = TRUE) |> 
-        purrr::map(read_yaml) |> 
-        data.table::rbindlist()
-
-data.table::fwrite(data, "data/fonte-recurso-hist.csv")
-data.table::fwrite(data[data$dt_fim_vigencia == "9999-12"], "data/fonte-recurso.csv")
+data <- yaml::yaml.load_file("classificador/fonteRecurso/lookup.yaml") |> data.table::rbindlist()
+data.table::fwrite(data, "data/fonte-recurso-temporal.csv")
+data.table::fwrite(data[valid_to == "9999-12"], "data/fonte-recurso.csv")
