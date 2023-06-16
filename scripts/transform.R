b9b11af26a1ba620b34e450e87a7411b347ed010
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 18:22:50 -0300

    moving to production

--- scripts/transform.R
+++ scripts/transform.R
@@ -1,3 +1,2 @@
 data <- yaml::yaml.load_file("classificador/fonteRecurso/lookup.yaml") |> data.table::rbindlist()
-data.table::fwrite(data, "data/fonte-recurso-temporal.csv")
-data.table::fwrite(data[valid_to == "9999-12"], "data/fonte-recurso.csv")
+data.table::fwrite(data, "data/fonte_recurso.csv")
