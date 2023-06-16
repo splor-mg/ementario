e2ab5254c217fc100ce3133b9854d987e46b89c2
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 19:01:29 -0300

    generalize bulk insert

--- scripts/bulk_insert.R
+++ scripts/bulk_insert.R
@@ -1,27 +1,22 @@
 library(execucao); library(relatorios); library(data.table); library(yaml)
 
-data("exec_rec")
-exec_rec <- exec_rec[, .N, .(ANO, MES_COD, FONTE_COD)]
-exec_rec <- adiciona_desc(exec_rec)
-exec_rec <- exec_rec[, valid_ref := paste(ANO, formatC(MES_COD, width = 2, flag = "0"), sep = "-")]
-exec_rec <- exec_rec[, .(valid_ref, FONTE_COD, FONTE_DESC)]
+bulk_insert <- function(classificacao, cod, desc) {
+  data <- execucao::exec_rec[, .N, by = c("ANO", "MES_COD", cod)]
+  data <- relatorios::adiciona_desc(data)
+  data <- data[, valid_ref := paste(ANO, formatC(MES_COD, width = 2, flag = "0"), sep = "-")]
+  
+  data <- data[, .(valid_from = min(valid_ref), valid_to = max(valid_ref)), by = c(cod, desc)]
+  
+  names(data) <- c("cod", "desc", "valid_from", "valid_to")
+  
+  data[valid_to == "2023-00", valid_to := "9999-12"]
+  
+  data$cod <- as.integer(data$cod)
+  
+  data[, seq := seq_len(.N) , cod]
+  data.table::setcolorder(data, c("cod", "seq", "desc", "valid_from", "valid_to"))
+  
+  write_yaml(data[order(cod, seq)], paste0("classificador/", classificacao, "/data.yaml"), column.major = FALSE, indent.mapping.sequence = TRUE)
+}
 
-data("loa_rec")
-loa_rec <- loa_rec[, .N, .(ANO, FONTE_COD)]
-loa_rec <- adiciona_desc(loa_rec)
-loa_rec <- loa_rec[, valid_ref := paste(ANO, "00", sep = "-")]
-loa_rec <- loa_rec[, .(valid_ref, FONTE_COD, FONTE_DESC)]
-
-fontes <- rbind(exec_rec, loa_rec)
-fontes <- fontes[, .(valid_from = min(valid_ref), valid_to = max(valid_ref)), by = .(FONTE_COD, FONTE_DESC)][order(FONTE_COD)]
-names(fontes) <- c("cod", "desc", "valid_from", "valid_to")
-
-fontes[valid_to == "2023-00", valid_to := "9999-12"]
-
-fontes$cod <- as.integer(fontes$cod)
-
-fontes[, seq := seq_len(.N) , cod]
-setcolorder(fontes, c("cod", "seq", "desc", "valid_from", "valid_to"))
-
-write_yaml(fontes, "classificador/fonteRecurso/data.yaml", column.major = FALSE, indent.mapping.sequence = TRUE)
-
+bulk_insert("unidadeOrcamentaria", "UO_COD", "UO_SIGLA")
