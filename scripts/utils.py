6f307044d36d24335eb3ff1e4cf3bea4ca9a33fb
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-16 21:24:42 -0300

    add multiple columns from reference table

--- scripts/utils.py
+++ scripts/utils.py
@@ -16,19 +16,22 @@ def time_to_decimal(time_str):
     year, month = map(int, time_str.split('-'))
     return Decimal(year) + Decimal(month) / 13
 
-def non_equi_join(tab1, tab2, field_tab1, field_tab2, new_column):
-    for record1 in tab1:
+def non_equi_join(x, y, key, key_y, resource):
+    fields = set(y[0].keys()) - {'cod', 'seq', 'valid_from', 'valid_to'}
+    for row in x:
         matched = False
-        for record2 in tab2:
-            valid_from = time_to_decimal(record2['valid_from'])
-            valid_to = time_to_decimal(record2['valid_to'])
-            valid_ref = time_to_decimal(record1['valid_ref'])
-            if record1[field_tab1] == record2[field_tab2] and valid_from <= valid_ref <= valid_to:
-                yield {**record1, f'{new_column}_desc': record2['desc']}
+        for row_y in y:
+            valid_from = time_to_decimal(row_y['valid_from'])
+            valid_to = time_to_decimal(row_y['valid_to'])
+            valid_ref = time_to_decimal(row['valid_ref'])
+            if row[key] == row_y[key_y] and valid_from <= valid_ref <= valid_to:
+                desc = {f'{resource}_{field}':row_y[field] for field in fields}
+                yield {**row, **desc}
                 matched = True
                 break  # we found a match, so we can stop looking in tab2 for this record
         if not matched:
-            yield {**record1, f'{new_column}_desc': None}  # if no match was found in tab2, output the original record from tab1
+                desc = {f'{resource}_{field}':None for field in fields}
+                yield {**row, **desc}
 
 
 def enrich_resource(resource):
